c8ec88ff7058c5dafea63e3807eba93f5b17fc18
renuka2101476 <136619825+renuka2101476@users.noreply.github.com>
2023-06-19 23:10:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var screen = document.querySelector('#screen');
+    var btn = document.querySelectorAll('.btn');
+
+  
+    for (item of btn) {
+        item.addEventListener('click', (e) => {
+            btntext = e.target.innerText;
+
+            if (btntext == '×') {
+                btntext = '*';
+            }
+
+            if (btntext == '÷') {
+                btntext = '/';
+            }
+            screen.value += btntext;
+        });
+    }
+
+    function fact() {
+        var i, num, f;
+        f = 1
+        num = screen.value;
+        for (i = 1; i <= num; i++) {
+            f = f * i;
+        }
+
+        i = i - 1;
+
+        screen.value = f;
+    }
+
+    function backspc() {
+      screen.value = screen.value.substr(0, screen.value.length - 1);
+  }
